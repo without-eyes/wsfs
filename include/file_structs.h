7419cafe1f60f23ab1e82faa8b459100c002a5ee
without-eyes <dimka180205@gmail.com>
2025-01-19 17:59:11 +0200

    Update FileAttributes struct with directory/file content, set max name size

--- include/file_structs.h
+++ include/file_structs.h
@@ -2,13 +2,18 @@
 #define FILE_H
 
 #include <stdint.h>
+#define MAX_FILE_NAME 32
 
 struct FileAttributes {
-    char type;
     uint64_t size;
+    char name[MAX_FILE_NAME];
+    union {
+        struct FileNode* directoryContent;
+        char* fileContent;
+    };
     uint8_t hourCreatedAt;
     uint8_t minuteCreatedAt;
-    char* name;
+    char type;
 };
 
 struct FileNode {
