85b648face68469ca212683c344cbfc60a747e34
without-eyes <dimka180205@gmail.com>
2025-01-19 17:58:02 +0200

    Add FileNode struct for tree nodes

--- include/file_structs.h
+++ include/file_structs.h
@@ -4,11 +4,16 @@
 #include <stdint.h>
 
 struct FileAttributes {
- 	char type;
-  	uint64_t size;
+    char type;
+    uint64_t size;
     uint8_t hourCreatedAt;
     uint8_t minuteCreatedAt;
     char* name;
+};
+
+struct FileNode {
+    struct FileAttributes attributes;
+    struct FileNode* parent;
 };
 
 #endif //FILE_H
