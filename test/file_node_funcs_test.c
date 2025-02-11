c1feaaa48b58bfab4b5b06d155ea9c63619fad2d
without-eyes <dimka180205@gmail.com>
2025-02-11 13:49:17 +0200

    Add unit tests for file_node_funcs.c

---
+++ test/file_node_funcs_test.c
@@ -0,0 +1,326 @@
+/**
+    * @file: file_node_funcs_test.c
+    * @author: without eyes
+    *
+    * This file contains tests for functions related
+    * to files and file nodes.
+*/
+
+#include "../include/file_node_funcs.h"
+
+#include <stdio.h>
+
+#include "criterion/criterion.h"
+#include "../include/utils.h"
+
+Test(create_file_node, basic_creation) {
+    struct FileNode* parent = create_file_node(NULL, "\\", FILE_TYPE_DIR);
+    char name[5] = "node";
+    enum FileType type = FILE_TYPE_FILE;
+
+    struct FileNode* child = create_file_node(parent, name, type);
+
+    struct Timestamp timestamp = get_current_time();
+    cr_assert_str_eq(child->attributes.name,            name);
+    cr_assert_eq(child->attributes.createdAt.hour,      timestamp.hour);
+    cr_assert_eq(child->attributes.createdAt.minute,    timestamp.minute);
+    cr_assert_eq(child->attributes.type,                type);
+    cr_assert_eq(child->parent,                         parent);
+    cr_assert_null(child->attributes.directoryContent);
+    cr_assert_null(child->attributes.fileContent);
+    cr_assert_null(child->attributes.symlinkTarget);
+    cr_assert_null(child->next);
+
+    free(child->attributes.name);
+    free(child);
+    free(parent->attributes.name);
+    free(parent);
+}
+
+Test(create_file_node, root_creation) {
+    char name[2] = "\\";
+    enum FileType type = FILE_TYPE_DIR;
+
+    struct FileNode* root = create_file_node(NULL, name, type);
+
+    struct Timestamp timestamp = get_current_time();
+    cr_assert_str_eq(root->attributes.name,             name);
+    cr_assert_eq(root->attributes.createdAt.hour,       timestamp.hour);
+    cr_assert_eq(root->attributes.createdAt.minute,     timestamp.minute);
+    cr_assert_eq(root->attributes.type,                 type);
+    cr_assert_eq(root->parent,                          root);
+    cr_assert_null(root->attributes.directoryContent);
+    cr_assert_null(root->attributes.fileContent);
+    cr_assert_null(root->attributes.symlinkTarget);
+    cr_assert_null(root->next);
+
+    free(root->attributes.name);
+    free(root);
+}
+
+Test(change_current_dir, dir_exists) {
+    struct FileNode* currentDir = create_file_node(NULL, "\\", FILE_TYPE_DIR);
+    struct FileNode* newCurrentDir = create_file_node(currentDir, "dir", FILE_TYPE_DIR);
+    add_to_dir(currentDir, newCurrentDir);
+
+    change_current_dir(&currentDir, newCurrentDir->attributes.name);
+
+    cr_assert_eq(currentDir, newCurrentDir);
+    cr_assert_str_eq(currentDir->attributes.name,           newCurrentDir->attributes.name);
+    cr_assert_eq(currentDir->attributes.createdAt.hour,     newCurrentDir->attributes.createdAt.hour);
+    cr_assert_eq(currentDir->attributes.createdAt.minute,   newCurrentDir->attributes.createdAt.minute);
+    cr_assert_eq(currentDir->attributes.type,               newCurrentDir->attributes.type);
+    cr_assert_eq(currentDir->attributes.directoryContent,   newCurrentDir->attributes.directoryContent);
+    cr_assert_eq(currentDir->attributes.fileContent,        newCurrentDir->attributes.fileContent);
+    cr_assert_eq(currentDir->attributes.symlinkTarget,      newCurrentDir->attributes.symlinkTarget);
+    cr_assert_eq(currentDir->next,                          newCurrentDir->next);
+    cr_assert_eq(currentDir->parent,                        newCurrentDir->parent);
+
+    free(currentDir->parent->attributes.name);
+    free(currentDir->parent);
+    free(currentDir->attributes.name);
+    free(currentDir);
+}
+
+Test(change_current_dir, dir_not_exists) {
+    struct FileNode* currentDir = create_file_node(NULL, "\\", FILE_TYPE_DIR);
+
+    change_current_dir(&currentDir, "not_exists");
+
+    cr_assert_str_eq(currentDir->attributes.name,           currentDir->attributes.name);
+    cr_assert_eq(currentDir->attributes.createdAt.hour,     currentDir->attributes.createdAt.hour);
+    cr_assert_eq(currentDir->attributes.createdAt.minute,   currentDir->attributes.createdAt.minute);
+    cr_assert_eq(currentDir->attributes.type,               currentDir->attributes.type);
+    cr_assert_eq(currentDir->attributes.directoryContent,   currentDir->attributes.directoryContent);
+    cr_assert_eq(currentDir->attributes.fileContent,        currentDir->attributes.fileContent);
+    cr_assert_eq(currentDir->attributes.symlinkTarget,      currentDir->attributes.symlinkTarget);
+    cr_assert_eq(currentDir->next,                          currentDir->next);
+    cr_assert_eq(currentDir->parent,                        currentDir->parent);
+
+    free(currentDir->attributes.name);
+    free(currentDir);
+}
+
+Test(change_current_dir, symlink) {
+    struct FileNode* currentDir = create_file_node(NULL, "\\", FILE_TYPE_DIR);
+    struct FileNode* symlink = create_file_node(currentDir, "symlink", FILE_TYPE_SYMLINK);
+    struct FileNode* newCurrentDir = create_file_node(currentDir, "dir", FILE_TYPE_DIR);
+    add_to_dir(currentDir, symlink);
+    add_to_dir(currentDir, newCurrentDir);
+    symlink->attributes.symlinkTarget = newCurrentDir;
+
+    change_current_dir(&currentDir, symlink->attributes.name);
+
+    cr_assert_str_eq(currentDir->attributes.name,           newCurrentDir->attributes.name);
+    cr_assert_eq(currentDir->attributes.createdAt.hour,     newCurrentDir->attributes.createdAt.hour);
+    cr_assert_eq(currentDir->attributes.createdAt.minute,   newCurrentDir->attributes.createdAt.minute);
+    cr_assert_eq(currentDir->attributes.type,               newCurrentDir->attributes.type);
+    cr_assert_eq(currentDir->attributes.directoryContent,   newCurrentDir->attributes.directoryContent);
+    cr_assert_eq(currentDir->attributes.fileContent,        newCurrentDir->attributes.fileContent);
+    cr_assert_eq(currentDir->attributes.symlinkTarget,      newCurrentDir->attributes.symlinkTarget);
+    cr_assert_eq(currentDir->next,                          newCurrentDir->next);
+    cr_assert_eq(currentDir->parent,                        newCurrentDir->parent);
+
+    free(currentDir->parent->attributes.name);
+    free(currentDir->parent);
+    free(currentDir->attributes.name);
+    free(currentDir);
+}
+
+Test(get_file_type_letter, all_types) {
+    cr_assert_eq(get_file_type_letter(FILE_TYPE_DIR),       'd');
+    cr_assert_eq(get_file_type_letter(FILE_TYPE_FILE),      'f');
+    cr_assert_eq(get_file_type_letter(FILE_TYPE_SYMLINK),   's');
+    cr_assert_eq(get_file_type_letter(FILE_TYPE_UNKNOWN),   '-');
+    cr_assert_eq(get_file_type_letter(999),                 '?');
+}
+
+Test(get_symlink_target, basic) {
+    struct FileNode* symlink = create_file_node(NULL, "symlink", FILE_TYPE_SYMLINK);
+    struct FileNode* target = create_file_node(NULL, "file", FILE_TYPE_FILE);
+    symlink->attributes.symlinkTarget = target;
+
+    const struct FileNode* result = get_symlink_target(symlink);
+
+    cr_assert_eq(result, target);
+
+    free(target->attributes.name);
+    free(target);
+    free(symlink->attributes.name);
+    free(symlink);
+}
+
+Test(get_symlink_target, symlink_is_null) {
+    const struct FileNode* result = get_symlink_target(NULL);
+
+    cr_assert_null(result);
+}
+
+Test(get_symlink_target, target_is_null) {
+    struct FileNode* symlink = create_file_node(NULL, "symlink", FILE_TYPE_SYMLINK);
+
+    const struct FileNode* result = get_symlink_target(symlink);
+
+    cr_assert_null(result);
+
+    free(symlink->attributes.name);
+    free(symlink);
+}
+
+Test(get_symlink_target, symlink_on_symlink) {
+    struct FileNode* firstSymlink = create_file_node(NULL, "symlink1", FILE_TYPE_SYMLINK);
+    struct FileNode* secondSymlink = create_file_node(NULL, "symlink2", FILE_TYPE_SYMLINK);
+    struct FileNode* target = create_file_node(NULL, "file", FILE_TYPE_FILE);
+    firstSymlink->attributes.symlinkTarget = secondSymlink;
+    secondSymlink->attributes.symlinkTarget = target;
+
+    const struct FileNode* result = get_symlink_target(firstSymlink);
+
+    cr_assert_eq(result, target);
+
+    free(target->attributes.name);
+    free(target);
+    free(secondSymlink->attributes.name);
+    free(secondSymlink);
+    free(firstSymlink->attributes.name);
+    free(firstSymlink);
+}
+
+Test(write_to_file, basic) {
+    struct FileNode* node = create_file_node(NULL, "file", FILE_TYPE_FILE);
+    char content[] = "content";
+
+    write_to_file(node, content);
+
+    cr_assert_str_eq(node->attributes.fileContent, content);
+
+    free(node->attributes.name);
+    free(node);
+}
+
+Test(write_to_file, node_or_content_is_null) {
+    struct FileNode* node = create_file_node(NULL, "file", FILE_TYPE_FILE);
+
+    write_to_file(node, NULL);
+    cr_assert_eq(node->attributes.fileContent, NULL);
+
+    write_to_file(NULL, "content");
+
+    free(node->attributes.name);
+    free(node);
+}
+
+Test(write_to_file, symlink) {
+    struct FileNode* symlink = create_file_node(NULL, "symlink", FILE_TYPE_SYMLINK);
+    struct FileNode* target = create_file_node(NULL, "file", FILE_TYPE_FILE);
+    symlink->attributes.symlinkTarget = target;
+    char content[] = "content";
+
+    write_to_file(symlink, content);
+
+    cr_assert_str_eq(target->attributes.fileContent, content);
+
+    free(target->attributes.name);
+    free(target);
+    free(symlink->attributes.name);
+    free(symlink);
+}
+
+Test(read_file_content, basic) {
+    struct FileNode* file = create_file_node(NULL, "file", FILE_TYPE_FILE);
+    char content[] = "content";
+    write_to_file(file, content);
+
+    cr_assert_str_eq(read_file_content(file), content);
+
+    free(file->attributes.name);
+    free(file);
+}
+
+Test(read_file_content, node_is_null) {
+    cr_assert_null(read_file_content(NULL));
+}
+
+Test(find_file_node_in_curr_dir, basic) {
+    struct FileNode* dir = create_file_node(NULL, "dir", FILE_TYPE_DIR);
+    struct FileNode* file = create_file_node(NULL, "file", FILE_TYPE_FILE);
+    add_to_dir(dir, file);
+
+    cr_assert_eq(find_file_node_in_curr_dir(dir, file->attributes.name), file);
+
+    free(file->attributes.name);
+    free(file);
+    free(dir->attributes.name);
+    free(dir);
+}
+
+Test(find_file_node_in_curr_dir, dir_or_name_is_null) {
+    cr_assert_null(find_file_node_in_curr_dir(NULL, "test"));
+    cr_assert_null(find_file_node_in_curr_dir((struct FileNode*)1, NULL));
+}
+
+Test(find_file_node_in_curr_dir, find_non_existing_file) {
+    struct FileNode* dir = create_file_node(NULL, "dir", FILE_TYPE_DIR);
+    cr_assert_null(find_file_node_in_curr_dir(dir, "not exist"));
+
+    free(dir->attributes.name);
+    free(dir);
+}
+
+Test(find_file_node_in_fs, find_existing_file) {
+    struct FileNode* root = create_file_node(NULL, "\\", FILE_TYPE_DIR);
+
+    cr_assert_null(find_file_node_in_fs(root, "not exist"));
+
+    free(root->attributes.name);
+    free(root);
+}
+
+Test(find_file_node_in_fs, root_or_name_is_null) {
+    cr_assert_null(find_file_node_in_fs(NULL, "test"));
+    cr_assert_null(find_file_node_in_fs((struct FileNode*)1, NULL));
+}
+
+Test(get_file_node_path, basic) {
+    struct FileNode* root = create_file_node(NULL, "\\", FILE_TYPE_DIR);
+    struct FileNode* file = create_file_node(root, "file", FILE_TYPE_FILE);
+    add_to_dir(root, file);
+
+    char* path = get_file_node_path(root, "file");
+    cr_assert_str_eq(path, "\\file");
+
+    free(path);
+    free(file->attributes.name);
+    free(file);
+    free(root->attributes.name);
+    free(root);
+}
+
+Test(get_file_node_path, dir_or_name_is_null) {
+    cr_assert_null(get_file_node_path(NULL, "file"));
+    cr_assert_null(get_file_node_path((struct FileNode*)1, NULL));
+}
+
+Test(change_file_node_name, rename) {
+    struct FileNode* file = create_file_node(NULL, "old", FILE_TYPE_FILE);
+
+    change_file_node_name(file, "new");
+
+    cr_assert_str_eq(file->attributes.name, "new");
+
+    free(file->attributes.name);
+    free(file);
+}
+
+Test(delete_file_node, delete_existing) {
+    struct FileNode* dir = create_file_node(NULL, "dir", FILE_TYPE_DIR);
+    struct FileNode* file = create_file_node(dir, "file", FILE_TYPE_FILE);
+    add_to_dir(dir, file);
+
+    delete_file_node(dir, "file");
+
+    cr_assert_null(dir->attributes.directoryContent);
+
+    free(dir->attributes.name);
+    free(dir);
+}
--- test/utils_test.c
+++ test/utils_test.c
@@ -19,6 +19,6 @@ Test(get_current_time, basic) {
     const struct Timestamp currentTime1 = { timeInfo->tm_hour, timeInfo->tm_min };
     const struct Timestamp currentTime2 = get_current_time();
 
-    cr_assert_eq(currentTime1.hour, currentTime2.hour);
-    cr_assert_eq(currentTime1.minute, currentTime2.minute);
+    cr_assert_eq(currentTime1.hour,     currentTime2.hour);
+    cr_assert_eq(currentTime1.minute,   currentTime2.minute);
 }
