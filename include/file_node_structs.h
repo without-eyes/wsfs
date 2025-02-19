43136264eb65ebf1d3d243b13f76c467e30c3771
without-eyes <dimka180205@gmail.com>
2025-02-19 18:06:31 +0200

    Add documentation for structs and enums

--- include/file_node_structs.h
+++ include/file_node_structs.h
@@ -10,54 +10,87 @@
 
 #include <stdint.h>
 
+/**
+ * @enum FileType
+ * @brief Represents different types of files.
+ */
 enum __attribute__ ((__packed__)) FileType {
-    FILE_TYPE_UNKNOWN = 0,
-    FILE_TYPE_FILE = 1,
-    FILE_TYPE_DIR = 2,
-    FILE_TYPE_SYMLINK = 3
+    FILE_TYPE_UNKNOWN = 0, /**< Unknown file type */
+    FILE_TYPE_FILE = 1,    /**< Regular file */
+    FILE_TYPE_DIR = 2,     /**< Directory */
+    FILE_TYPE_SYMLINK = 3  /**< Symbolic link */
 };
 
+/**
+ * @enum Permissions
+ * @brief Defines file access permissions.
+ */
 enum __attribute__ ((__packed__)) Permissions {
-    PERM_NONE = 0,
-    PERM_EXEC = 1,
-    PERM_WRITE = 2,
-    PERM_READ = 4
+    PERM_NONE = 0,  /**< No permissions */
+    PERM_EXEC = 1,  /**< Execute permission */
+    PERM_WRITE = 2, /**< Write permission */
+    PERM_READ = 4   /**< Read permission */
 };
 
-struct FileNode;
+struct FileNode; /**< Forward declaration of FileNode struct */
 
+/**
+ * @struct Timestamp
+ * @brief Represents a time value with hour and minute.
+ */
 struct Timestamp {
-    uint8_t hour, minute;
+    uint8_t hour;   /**< Hour value (0-23) */
+    uint8_t minute; /**< Minute value (0-59) */
 };
 
+/**
+ * @struct FileMetadata
+ * @brief Contains metadata related to a file.
+ */
 struct FileMetadata {
-    char* name;
-    struct Timestamp creationTime;
+    char* name;                     /**< Name of the file */
+    struct Timestamp creationTime;  /**< Timestamp of file creation */
 };
 
+/**
+ * @struct FileProperties
+ * @brief Stores file type and permissions.
+ */
 struct FileProperties {
-    enum FileType type;
-    enum Permissions permissions;
+    enum FileType type;             /**< Type of the file */
+    enum Permissions permissions;   /**< File permissions */
 };
 
+/**
+ * @struct FileData
+ * @brief Represents the content of a file.
+ */
 struct FileData {
     union {
-        struct FileNode* directoryContent;
-        struct FileNode* symlinkTarget;
-        char* fileContent;
+        struct FileNode* directoryContent; /**< Pointer to directory content (if directory) */
+        struct FileNode* symlinkTarget;    /**< Pointer to symbolic link target (if symlink) */
+        char* fileContent;                 /**< Pointer to file content (if regular file) */
     };
 };
 
+/**
+ * @struct FileInfo
+ * @brief Holds complete information about a file.
+ */
 struct FileInfo {
-    struct FileMetadata metadata;
-    struct FileProperties properties;
-    struct FileData data;
+    struct FileMetadata metadata;       /**< Metadata of the file */
+    struct FileProperties properties;   /**< Properties such as type and permissions */
+    struct FileData data;               /**< File data/content */
 };
 
+/**
+ * @struct FileNode
+ * @brief Represents a file node in the file system.
+ */
 struct FileNode {
-    struct FileInfo info;
-    struct FileNode* parent;
-    struct FileNode* next;
+    struct FileInfo info;      /**< Information about the file */
+    struct FileNode* parent;   /**< Pointer to the parent node */
+    struct FileNode* next;     /**< Pointer to the next node */
 };
 
 #endif //FILE_NODE_STRUCTS_H
